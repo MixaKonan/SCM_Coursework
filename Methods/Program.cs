e72320e50d742c9943c332f27c2a534e36463f5f
Mykhailo Konontsev <fater181@gmail.com>
2021-11-25 18:48:00 +0200

    Added Taylor series approximation

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Methods
 {
@@ -9,81 +10,49 @@ namespace Methods
     {
         public static void Main(string[] args)
         {
-            var mySinValues = new Dictionary<double, double>();
-            var mathSinValues = new Dictionary<double, double>();
-            var errors = new Dictionary<double, double>();
+            var chebyshev = new Dictionary<double, double>();
+            var taylor = new Dictionary<double, double>();
+            var mathLib = new Dictionary<double, double>();
+            
+            var chebyshevErrors = new Dictionary<double, double>();
+            var taylorErrors = new Dictionary<double, double>();
 
-            for (var x = -Math.PI; x < Math.PI; x += 0.001)
+            for (var x = -1d; x < 1; x += 0.001)
             {
-                var myVal = Sin(x);
-                var libVal = Math.Sin(x);
+                var cheb = SinApproximation.Chebyshev(x);
+                var tayl = SinApproximation.Taylor(x, 6);
+                var lib = Math.Sin(x);
 
-                var error = (myVal / libVal) - 1;
+                var errorCheb = (cheb / lib) - 1;
+                var errorTayl = (tayl / lib) - 1;
                 
-                mySinValues.Add(x, myVal);
-                mathSinValues.Add(x, libVal);
-                errors.Add(x, error);
+                chebyshev.Add(x, cheb);
+                taylor.Add(x, tayl);
+                mathLib.Add(x, lib);
+                
+                chebyshevErrors.Add(x, errorCheb);
+                taylorErrors.Add(x, errorTayl);
             }
-            
-            using (var stream = File.Create("../../../my_sin.csv"))
-            {
-                using var writer = new StreamWriter(stream);
-                
-                //writer.WriteLine("X;Y");
-                
-                foreach (var (key, value) in mySinValues)
-                {
-                    writer.WriteLine($"{key.ToString(CultureInfo.InvariantCulture)};{value.ToString(CultureInfo.InvariantCulture)}");
-                }
-            }
-            
-            using (var stream = File.Create("../../../math_sin.csv"))
-            {
-                using var writer = new StreamWriter(stream);
-                
-                //writer.WriteLine("X;Y");
-                
-                foreach (var (key, value) in mathSinValues)
-                {
-                    writer.WriteLine($"{key.ToString(CultureInfo.InvariantCulture)};{value.ToString(CultureInfo.InvariantCulture)}");
-                }
-            }
-            
-            using (var stream = File.Create("../../../errors.csv"))
-            {
-                using var writer = new StreamWriter(stream);
-                
-                //writer.WriteLine("X;Y");
-                
-                foreach (var (key, value) in errors)
-                {
-                    writer.WriteLine($"{key.ToString(CultureInfo.InvariantCulture)};{value.ToString(CultureInfo.InvariantCulture)}");
-                }
-            }
+
+            WriteResultsToFile(chebyshevErrors, "chebyshev_errors.csv");
+            WriteResultsToFile(taylorErrors, "taylor_errors.csv");
+            WriteResultsToFile(mathLib, "lib.csv");
+            WriteResultsToFile(chebyshev, "chebyshev.csv");
+            WriteResultsToFile(taylor, "taylor.csv");
         }
 
-        public static double Sin(double x)
+        private static void WriteResultsToFile(Dictionary<double, double> content, string filename)
         {
-            double[] coefficients =
+            using (var stream = File.Create($"../../../{filename}"))
             {
-                0.999999999973088, // x
-                -0.1666666663960699, // x^3
-                0.00833333287058762, // x^5
-                -0.0001984123883227529, // x^7,
-                2.755627491096882e-6, // x^9
-                -2.503262029557047e-8, // x^11
-                1.58535563425041e-10 //x^13
-            };
-
-            var p13 = coefficients[6];
-            var p11 = p13 * x * x + coefficients[5];
-            var p9 = p11 * x * x + coefficients[4];
-            var p7 = p9 * x * x + coefficients[3];
-            var p5 = p7 * x * x + coefficients[2];
-            var p3 = p5 * x * x + coefficients[1];
-            var p1 = p3 * x * x + coefficients[0];
-
-            return p1 * x;
+                using (var writer = new StreamWriter(stream))
+                {
+                    foreach (var (key, value) in content)
+                    {
+                        writer.WriteLine($"{key.ToString(CultureInfo.InvariantCulture)};{value.ToString(CultureInfo.InvariantCulture)}");
+                    }
+                }
+            }
         }
     }
 }
--- sine_plotting.py
+++ sine_plotting.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy
 
 
 def get_values(filename: str) -> list[list]:
@@ -15,25 +16,60 @@ def get_values(filename: str) -> list[list]:
 
     return [x_values, y_values]
 
-my_sin_values = get_values("E:\Studying\SCM_Coursework\Methods\my_sin.csv")
-math_sin_values = get_values("E:\Studying\SCM_Coursework\Methods\math_sin.csv")
-error_values = get_values("E:\Studying\SCM_Coursework\Methods\errors.csv")
+chebyshes_values = get_values("E:\Studying\SCM_Coursework\Methods\chebyshev.csv")
+taylor_values = get_values("E:\Studying\SCM_Coursework\Methods\\taylor.csv")
+math_sin_values = get_values("E:\Studying\SCM_Coursework\Methods\lib.csv")
+
+chebyshev_error = get_values("E:\Studying\SCM_Coursework\Methods\chebyshev_errors.csv")
+taylor_error = get_values("E:\Studying\SCM_Coursework\Methods\\taylor_errors.csv")
+
+figure, plots = plt.subplots(3, 2)
+
+chebyshev_plot = plots[0, 0]
+taylor_plot = plots[1, 0]
+library_plot = plots[2, 0]
+
+chebyshev_error_plot = plots[0, 1]
+taylor_error_plot = plots[1, 1]
+
+chebyshev_plot.grid()
+chebyshev_plot.set_title("Chebyshev")
 
 
-plt.figure(0)
-plt.grid()
-plt.plot(my_sin_values[0], my_sin_values[1], label="Chebyshev coefficients")
-plt.plot(math_sin_values[0], math_sin_values[1], label="Built-in sin(x) function")
-plt.xlabel("Degrees (radian)")
-plt.ylabel("Sin(x)")
-plt.legend(fontsize=10)
+taylor_plot.grid()
+taylor_plot.set_title("Taylor")
 
-plt.figure(1)
-plt.grid()
-plt.plot(error_values[0], error_values[1], label='Error')
-plt.xlabel('Degrees (radian)')
-plt.ylabel("Error")
-plt.legend(fontsize=10)
+
+library_plot.grid()
+library_plot.set_title("Library")
+
+
+chebyshev_error_plot.grid()
+chebyshev_error_plot.set_title("Chebyshev error")
+
+taylor_error_plot.grid()
+taylor_error_plot.set_title("Taylor error")
+
+chebyshev_plot.plot(chebyshes_values[0], chebyshes_values[1], label="Chebyshev coefficients", color="green")
+taylor_plot.plot(taylor_values[0], taylor_values[1], label="Taylor series", color="red")
+library_plot.plot(math_sin_values[0], math_sin_values[1], label="Built-in sin(x) function", color="blue")
+
+#chebyshev_plot.xlabel("Degrees (radian)")
+#chebyshev_plot.ylabel("Sin(x)")
+#taylor_plot.xlabel("Degrees (radian)")
+#taylor_plot.ylabel("Sin(x)")
+#library_plot.xlabel("Degrees (radian)")
+#library_plot.ylabel("Sin(x)")
+
+#values.legend(fontsize=10)
+
+#plots[0, 1].grid()
+chebyshev_error_plot.plot(chebyshev_error[0], chebyshev_error[1], label='Chebyshev error', color="green")
+taylor_error_plot.plot(taylor_error[0], taylor_error[1], label='Taylor error', color="red")
+#plots[2, 1].plot(0, 0, label='Taylor error')
+#errors.xlabel('Degrees (radian)')
+#errors.ylabel("Error")
+#errors.legend(fontsize=10)
 
 plt.show()
 
