25c1d57445b4c751357dcf1425de18b39b88782a
Mykhailo Konontsev <mykhailo.konontsev@perfectial.com>
2021-12-07 19:20:57 +0200

    Added classic range reduction

---
+++ Methods/RangeReduction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Methods
+{
+    public class RangeReduction
+    {
+        //x' = x - kC
+
+        public const double C = Math.PI / 4;
+        public static double ClassicReduction(double x, double c)
+        {
+            double k = Math.Floor(x / c);
+
+            return x - k * c;
+        }
+    }
+}
